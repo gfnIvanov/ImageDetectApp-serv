7b27f2bc2a1503e286e801aac3334bf3038a8113
Игорь Иванов <is.ivanov@bars.group>
2023-12-04 18:55:08 +0300

    Use model with frontend

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,14 @@ app = Flask(__name__)
 
 CORS(app)
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-CHECK_IMG_DIR = os.path.join(BASE_DIR, "data/for_check")
-MODEL_FILE = os.path.join(BASE_DIR, "models/model.pth")
-MODEL_PARAMS = os.path.join(BASE_DIR, "models/params.txt")
+app.config['BASE_DIR'] = Path(__file__).resolve().parent.parent
+app.config['CHECK_IMG_DIR'] = os.path.join(app.config['BASE_DIR'], "data/for_check")
+app.config['UPLOAD_DIR'] = os.path.join(app.config['BASE_DIR'], "data/for_check")
+app.config['MODEL_FILE'] = os.path.join(app.config['BASE_DIR'], "models/model.pth")
+app.config['MODEL_PARAM'] = os.path.join(app.config['BASE_DIR'], "models/params.txt")
 
 logging.basicConfig(level = logging.DEBUG,
-                    filename = os.path.join(BASE_DIR, 'logs/app_log.log'),
+                    filename = os.path.join(app.config['BASE_DIR'], 'logs/app_log.log'),
                     filemode = "w",
                     format = "%(asctime)s - %(name)s[%(funcName)s(%(lineno)d)] - %(levelname)s - %(message)s")
 
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,10 @@
 import os
-from app import (
-    app, 
-    BASE_DIR, 
-    MODEL_FILE, 
-    MODEL_PARAMS, 
-    model
-)
+import shutil
+from app import app, model
 import logging
 from flask import request, make_response
 from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
 
 
 load_dotenv()
@@ -22,10 +18,10 @@ def index():
         train_params = ""
         status = 404
 
-        if os.path.exists(MODEL_FILE):
+        if os.path.exists(app.config['MODEL_FILE']):
             status = 200
-            model_name = os.path.basename(MODEL_FILE)
-            with open(MODEL_PARAMS, "r") as file:
+            model_name = os.path.basename(app.config['MODEL_FILE'])
+            with open(app.config['MODEL_PARAM'], "r") as file:
                 param_lines = file.readlines()
                 model_date = param_lines[0]
                 train_params = param_lines[1]
@@ -45,8 +41,17 @@ def index():
 @app.route("/use-model", methods=["PUT"])
 def use_model():
     try:
-       model.use()
-       return make_response("OK", 200)
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        classname = request.form['class']
+        if os.path.exists(app.config['UPLOAD_DIR']):
+            shutil.rmtree(app.config['UPLOAD_DIR'])
+            os.mkdir(app.config['UPLOAD_DIR'])
+        class_dir = os.path.join(app.config['UPLOAD_DIR'], classname)
+        os.mkdir(class_dir)
+        file.save(os.path.join(class_dir, filename))
+        result = model.use()
+        return make_response(result, 200)
     except Exception as err:
         app_log.error(err)
         return make_response(err, 500)
